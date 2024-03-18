c6c496dba7235a24126d88892a2d3fb5d2a2b475
Abhijeetsabale12 <111568849+Abhijeetsabale12@users.noreply.github.com>
2024-03-18 12:22:35 +0530

    from system

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,11 @@
 import express,{Request, Response} from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoutes"
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>
+console.log("Connected to Database"))
 
 const app = express();
 app.use(express.json())
@@ -9,6 +14,8 @@ app.use(cors())
 app.get("/test", async (req: Request, res:Response ) => {
     res.json({message: "Hello!!!!!"});
 });
+
+app.use("/api/my/user", myUserRoute)
 
 app.listen(7000, ()=>{
     console.log("server started on localhost:7000");
